13dfdf04dcbdfaa8c794b5cb6336fdf0cc5d3ce6
Devang Bhagdev <devang@webcodegenie.com>
2020-05-12 13:43:40 +0530

    initial commit

---
+++ requirements/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    site_title: "Day Length Demo", //site title for display title in head section of html
+    parallelReq: 5, //varible for number pf parallel request for get sunrise sunset data
+    messages: { //messages for api response
+        "001" : "Get sunrice / sunset timings successfully",
+        "002" : "Send proper lat long data",
+        "003" : "Get earliest sunrise timings successfully",
+        "004" : "Send proper timing data",
+    }
+}
